5fff50bf2a6347e6d7359c40a9ce25dc1177c16d
Shehan123-Rusiru <ss.rusiru@gmail.com>
2021-08-25 10:00:03 +0530

    First commit

---
+++ AppNET/ASP.NET/Data/StudentContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sheffield_App.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET.Data
+{
+   
+    public class StudentContext : DbContext
+    {
+        public StudentContext(DbContextOptions<StudentContext> options) : base(options){ }
+    
+        public DbSet<Student> students { get; set; }
+
+   
+    }
+}
